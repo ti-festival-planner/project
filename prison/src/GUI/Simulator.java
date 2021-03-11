3f6c2f5f0cf11ceefea4221a2341ea8c6106d815
Robin <luijtenrobin@gmail.com>
2021-03-11 10:11:04 +0100

    TileGUI

---
+++ prison/src/GUI/Simulator.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Simulator extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}
